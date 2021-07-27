9d12e9ebb55f652d51ce7e425116e032a74ec5ce
kang sung hwan <kumi1688@gmail.com>
2021-07-27 16:40:35 +0900

    minor fix

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const config = {
-    SERVER_IP: 'http://localhost',
+    SERVER_IP: 'http://210.107.206.176',
     PORT: 3001,
-    SMARTTHINGS_ACCESS_TOKEN: '8ec50db1-58bd-419c-852d-0936a03bfce4',
     URL: 'https://api.smartthings.com/v1/devices',
     MQTT_BROKER_IP: 'localhost',
     MQTT_BROKER_PORT: 1883,
